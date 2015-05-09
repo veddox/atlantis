2e3aca58caa9c14fd61850eed83e6bcc088823d3
Daniel Vedder <d.vedder@web.de>
2015-05-09 11:34:14 +0200

    First commit to the Lisp branch.
    Originally I had wanted to write Atlantis in Lisp. Due to various factors I
    decided to do the main implementation in Python, but I want to try in Lisp
    anyway.

---
+++ src/atlantis.lisp
@@ -0,0 +1,14 @@
+;;;
+;;; Atlantis is a framework for creating multi-user dungeon worlds.
+;;; This is the Common Lisp implementation.
+;;;
+;;; Licensed under the terms of the GPLv3
+;;; author: Daniel Vedder
+;;; date: 09/05/2015
+;;;
+
+(defun start-menu ()
+	"Show the start menu and take a choice from the user"
+	)
+
+(start-menu)
