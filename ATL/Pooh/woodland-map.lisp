15a857faf50d77c43ddb271ad626c806446e23cd
Daniel Vedder <d.vedder@web.de>
2017-07-31 10:34:00 +0200

    Added game map.

---
+++ ATL/Pooh/woodland-map.lisp
@@ -0,0 +1,42 @@
+; The 100 Acre Wood was invented by A.A. Milne for his Winnie-the-Pooh stories.
+; This Atlantis world is based on the novels.
+;
+; This file shows the game map.
+;
+; author: Daniel Vedder
+; date: 31/07/2017
+
+(defun map (player &optional arg)
+	"Print out the map"
+	(setf map-string "
+..............................[  100 ACRE WOODS  ]..............................
+:                                                                              :
+:                          Misty forest                      Rapids            :
+:                                                                              :
+:                                               Bee tree                       :
+:               Sandy pit                                                      :
+:                                                                              :
+:                                                 Rabbit                       :
+:       Kanga                      Northern                                    :
+:                                   woods                 Rabbit's             :
+:                 Stream              |              friends-and-relations     :
+:                                     |                                        :
+:                           Deep      |       Hill                             :
+:                          forest     |                                        :
+:                                     |                                        :
+: POOH'S       Western ___________ Central _____________ Eastern               :
+:  HOME         woods               woods                 woods                :
+:                                     |                                        :
+:                                     |       Owl                Christopher   :
+:             Six pine                |                             Robin      :
+:              trees                  |                                        :
+:                                     |                                        :
+:                                  Southern                                    :
+:       Piglet                      woods                                      :
+:                                                                              :
+:                                                                              :
+:                   Floody                                 Bridge              :
+:                   place                Eeyore                                :
+:..............................................................................:
+")
+	(format t "~A" map-string))
